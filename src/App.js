b70df2141ede265b4df9f91be776a3414555131f
toofaniCoder <smartcoder500@gmail.com>
2021-05-25 12:51:40 +0530

    add all starter files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "antd/dist/antd.css";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>ye Ant Design hai frandsssss.</h1>
+    </div>
+  );
+}
+
+export default App;
